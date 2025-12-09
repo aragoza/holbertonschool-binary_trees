15b381cb5a4287cc1300c18a4fb21ad2b09dd1e7
aragoza <charlet.elliot@gmail.com>
2025-12-09 13:22:11 +0100

    Write a function that checks if a given node is a root

--- binary_trees.h
+++ binary_trees.h
@@ -20,5 +20,8 @@ typedef struct binary_tree_s
 } binary_tree_t;
 
 void binary_tree_print(const binary_tree_t *);
+int binary_tree_is_leaf(const binary_tree_t *node);
+int binary_tree_is_root(const binary_tree_t *node);
+size_t binary_tree_height(const binary_tree_t *tree);
 
 #endif /* _BINARY_TREES_H_ */
