c109cb01230a4293904bff25f957d1fe1122dddc
aragoza <charlet.elliot@gmail.com>
2025-12-10 19:16:38 +0100

    better addition by one for the result height of a leaf

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -18,9 +18,13 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 	if (tree->left != NULL)
 		height_l = 1 + binary_tree_height(tree->left);
+	else
+		height_l = 0;
 
 	if (tree->right != NULL)
 		height_r = 1 + binary_tree_height(tree->right);
+	else
+		height_r = 0;
 
 	if (height_l > height_r)
 		return (height_l);
