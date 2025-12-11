2e51a098b75450aa3c1be75eaa4594e975a0697f
aragoza <charlet.elliot@gmail.com>
2025-12-11 10:05:49 +0100

    include the header file and append the doc

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -1,11 +1,11 @@
-#include
+#include "binary_trees.h"
 
 /**
+ * binary_tree_is_leaf - test if the node is a leaf
  *
+ * @node: is the node we want to check 
  *
- *
- *
- *
+ * Return: (1) on success (0) on fail
  */
 
 int binary_tree_is_leaf(const binary_tree_t *node)
