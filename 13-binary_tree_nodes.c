dcdb9f5d0b8284292c7587ccd55d156107a10aa4
aragoza <charlet.elliot@gmail.com>
2025-12-11 12:05:05 +0100

    append a better style for the function

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -13,7 +13,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	size_t count = 0;
 
 	if (tree == NULL)
-		return 0;
+		return (0);
 
 	if (tree->left != NULL || tree->right != NULL)
 		count = 1;
@@ -21,5 +21,5 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	count += binary_tree_nodes(tree->left);
 	count += binary_tree_nodes(tree->right);
 
-	return count;
+	return (count);
 }
