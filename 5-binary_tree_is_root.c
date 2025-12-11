b20dc6bfd2d75c3658234bd156c46975e3234bda
aragoza <charlet.elliot@gmail.com>
2025-12-11 11:57:26 +0100

    append the doc the header file and did the good style for betty

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,23 +1,21 @@
-#include
+#include "binary_trees.h"
+
 
 /**
+ * binary_tree_is_root - test if the node is the root
  *
+ * @node: is the node we want to check
  *
- *
- *
- *
- *
- *
- *
+ * Return: (1) on success (0) on fail
  */
 
 int binary_tree_is_root(const binary_tree_t *node)
 {
-    if(node == NULL)
-        return (0);
+	if (node == NULL)
+		return (0);
 
-    else if(node->parent == NULL)
-        return (1);
+	else if (node->parent == NULL)
+		return (1);
 
-    return (0);
+	return (0);
 }
