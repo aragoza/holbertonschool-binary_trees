0d4a4081e6932fadee33917e27db3f9f41bd18f6
aragoza <charlet.elliot@gmail.com>
2025-12-11 11:59:20 +0100

    append a better style for betty

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -3,17 +3,18 @@
 /**
  * binary_tree_is_leaf - test if the node is a leaf
  *
- * @node: is the node we want to check 
+ * @node: is the node we want to check
  *
  * Return: (1) on success (0) on fail
  */
 
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-    if(node == NULL)
-        return (0);
-    else if(node->left == NULL && node->right == NULL)
-        return (1);
-    
-    return (0);
+	if (node == NULL)
+		return (0);
+	
+	else if (node->left == NULL && node->right == NULL)
+		return (1);
+
+	return (0);
 }
